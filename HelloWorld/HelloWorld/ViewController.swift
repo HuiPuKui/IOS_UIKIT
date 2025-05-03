2979fe7ac477dc5f49aae2c0a096778db33abaa4
huipukui <huipukui@autohome.com.cn>
2025-05-03 22:38:55 +0800

    feat: 新建helloworld项目

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by HuiPuKui on 2025/5/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("hello")
+    }
+
+
+}
+
