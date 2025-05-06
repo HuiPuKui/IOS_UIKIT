ff54c307ff708b2559c76696d1f3ad3544880d5a
huipukui <1137438071@qq.com>
2025-05-06 16:36:16 +0800

    feat: print 与 debugPrint

--- 5-Swift语言-入门/注释,打印,控制台.playground/Contents.swift
+++ 5-Swift语言-入门/注释,打印,控制台.playground/Contents.swift
@@ -1,5 +1,7 @@
 import UIKit
 
+var greeting = "Hello, playground"
+
 // 1. 注释
 
 // 单行注释
@@ -7,3 +9,14 @@ import UIKit
 /*
  多行注释
  */
+
+// 2. 打印
+// print 是带换行符的
+print("hello\n")
+print("world\n")
+print(greeting)
+
+// debugPrint 打印原本内容
+debugPrint("123\n")
+
+
