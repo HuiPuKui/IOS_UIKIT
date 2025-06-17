9324edda4d980ec27e89e844c8714b0048bb2891
huipukui <1137438071@qq.com>
2025-06-18 00:34:48 +0800

    feat: categoryHeader ui

---
+++ 进阶版-2-外卖App点菜页/DoubleTableView/DoubleTableView/CategoryHeader.swift
@@ -0,0 +1,22 @@
+//
+//  CategoryHeader.swift
+//  DoubleTableView
+//
+//  Created by 惠蒲葵 on 2025/6/18.
+//
+
+import UIKit
+
+class CategoryHeader: UITableViewHeaderFooterView {
+
+    @IBOutlet weak var categoryNameLabel: UILabel!
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
