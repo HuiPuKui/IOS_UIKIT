bc4c53395597dda8693cfc000bc86ff6d0ce333a
huipukui <1137438071@qq.com>
2025-06-25 00:42:53 +0800

    feat: UICollectionView

--- README.md
+++ README.md
@@ -269,3 +269,5 @@ struct User {
 * internal: 默认权限，可以在同一个 Module 中使用
 * public: 可以在不同的 Module 中使用，public 的修饰内容在外部无法被继承或者重写
 * open: 可以在不同的 Module 中使用，open 的修饰内容在外部可以被继承或者重写，只能用来修饰 class，和 class 中的属性和方法
+
+## 4-UICollectionView
---
+++ 进阶版-4-UICollectionView/CollectionView/CollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionView
+//
+//  Created by 惠蒲葵 on 2025/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
