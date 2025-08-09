523ff7c03b64d7e4a42be0392c6b4e67505393b3
huipukui <1137438071@qq.com>
2025-08-09 20:12:49 +0800

    feat: 用代码指定所有navigationbar的效果

--- 进阶版-9-TabBarController/WeChatTabbarController/WeChatTabbarController/AppDelegate.swift
+++ 进阶版-9-TabBarController/WeChatTabbarController/WeChatTabbarController/AppDelegate.swift
@@ -14,6 +14,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let naviBarAppearance = UINavigationBarAppearance()
+        naviBarAppearance.configureWithDefaultBackground()
+        UINavigationBar.appearance().standardAppearance = naviBarAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = naviBarAppearance
+        
         return true
     }
 
