9d732cec665cc7cd2593fc8b2e48d73ac745eda6
huipukui <1137438071@qq.com>
2025-08-16 22:43:44 +0800

    feat: 添加 tableVIew

---
+++ 进阶版-11-用纯代码写一个小项目/JSONParsing/JSONParsing/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  JSONParsing
+//
+//  Created by 惠蒲葵 on 2025/8/16.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- 进阶版-11-用纯代码写一个小项目/JSONParsing/JSONParsing/ViewController.swift
+++ 进阶版-11-用纯代码写一个小项目/JSONParsing/JSONParsing/ViewController.swift
@@ -7,16 +7,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    // lazy: 只有使用到这个 UI 控件的时候才会调用这个代码
+    private lazy var tableView: UITableView = {
+        let tableView: UITableView = UITableView()
+        
+        // 设置属性
+        tableView.translatesAutoresizingMaskIntoConstraints = false // 用约束来指定布局
+        tableView.dataSource = self
+        tableView.register(TableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        return tableView
+    }()
     
     var courses: [Course] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
         loadData()
+        
+        self.view.addSubview(self.tableView)
+        setUI()
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        self.courses.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: TableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
+        
+        // 配置数据
+        var contentConfig = cell.defaultContentConfiguration()
+        contentConfig.text = courses[indexPath.row].title
+        
+        cell.contentConfiguration = contentConfig
+        
+        return cell
+    }
 
 }
 
@@ -39,6 +69,28 @@ extension ViewController {
         } else {
             print("从 courses.json 文件中取 url 失败，检查拼写等")
         }
+    }
+    
+}
+
+extension ViewController {
+    
+    func setUI() {
+        self.tableView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+//        self.tableView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor).isActive = true // 如果要针对 safe area
+        self.tableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+        self.tableView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        self.tableView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+        
+        // 等价于
+        /*
+        NSLayoutConstraint.activate([
+            self.tableView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            self.tableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            self.tableView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            self.tableView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor)
+        ])
+         */
     }
     
 }
