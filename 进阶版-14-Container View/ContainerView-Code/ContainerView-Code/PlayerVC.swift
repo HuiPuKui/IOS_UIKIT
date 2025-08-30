7e15b376667c5a2d70017e32662f15464cc3021b
huipukui <1137438071@qq.com>
2025-08-30 13:07:40 +0800

    feat: 补充 storyboard ID

---
+++ 进阶版-14-Container View/ContainerView-Code/ContainerView-Code/PlayerVC.swift
@@ -0,0 +1,18 @@
+//
+//  PlayerVC.swift
+//  ContainerView-Code
+//
+//  Created by 惠蒲葵 on 2025/8/30.
+//
+
+import UIKit
+import AVKit
+
+class PlayerVC: AVPlayerViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
---
+++ 进阶版-14-Container View/ContainerView-Code/ContainerView-Code/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContainerView-Code
+//
+//  Created by 惠蒲葵 on 2025/8/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
