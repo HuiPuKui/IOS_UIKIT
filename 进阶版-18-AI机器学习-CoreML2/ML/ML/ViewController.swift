9558f3440b3b1fc917e741fe322ca1f852767512
huipukui <1137438071@qq.com>
2025-09-13 19:53:47 +0800

    feat: 添加相机调用

--- 进阶版-18-AI机器学习-CoreML2/ML/ML/ViewController.swift
+++ 进阶版-18-AI机器学习-CoreML2/ML/ML/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func pick(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .camera
+        present(picker, animated: true, completion: nil)
+    }
+    
 }
 
+extension ViewController: UIImagePickerControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.originalImage] as? UIImage {
+            self.imageView.image = image
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+}
+
+extension ViewController: UINavigationControllerDelegate {
+    
+    
+    
+}
