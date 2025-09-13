a122b927710794ea1b4956d4a9234d029477d4d4
huipukui <1137438071@qq.com>
2025-09-13 22:46:45 +0800

    feat: 识别完整项目上传

--- 进阶版-18-AI机器学习-CoreML2/ML/ML/ViewController.swift
+++ 进阶版-18-AI机器学习-CoreML2/ML/ML/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController {
 
@@ -30,7 +32,39 @@ extension ViewController: UIImagePickerControllerDelegate {
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let image = info[.originalImage] as? UIImage {
             self.imageView.image = image
+            self.navigationItem.title = "识别中..."
+            
+            guard let ciImage = CIImage(image: image) else {
+                fatalError("不能把 UIImage 转化为 CIImage")
+            }
+            
+            guard let model = try? VNCoreMLModel(for: Inceptionv3(configuration: MLModelConfiguration()).model) else {
+                fatalError("加载 MLModel 失败")
+            }
+            
+            let request = VNCoreMLRequest(model: model) { request, error in
+                guard let res = request.results else {
+                    self.navigationItem.title = "图像识别失败"
+                    return
+                }
+                
+                let classification = res as! [VNClassificationObservation]
+                if classification.isEmpty {
+                    self.navigationItem.title = "不知道是什么"
+                } else {
+                    self.navigationItem.title = classification.first!.identifier
+                }
+            }
+            
+            request.imageCropAndScaleOption = .centerCrop
+            
+            do {
+                try VNImageRequestHandler(ciImage: ciImage).perform([request])
+            } catch {
+                print("执行图片识别请求失败，原因是: \(error.localizedDescription)")
+            }
         }
+        
         picker.dismiss(animated: true, completion: nil)
     }
     
