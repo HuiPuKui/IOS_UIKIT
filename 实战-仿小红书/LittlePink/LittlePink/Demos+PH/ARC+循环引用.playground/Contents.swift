c54601fc20e4deb424f88ea9b388683b5154c7ea
huipukui <1137438071@qq.com>
2025-11-16 10:16:33 +0800

    feat: 避免循环引用

--- 实战-仿小红书/LittlePink/LittlePink/Demos+PH/ARC+循环引用.playground/Contents.swift
+++ 实战-仿小红书/LittlePink/LittlePink/Demos+PH/ARC+循环引用.playground/Contents.swift
@@ -4,9 +4,9 @@ import UIKit
 // 内存管理机制
 
 // Reference Cycle-循环引用
-
+// 解决方法: weak(弱引用) 和 unowned(无主引用)
 class Video {
-    var author: Author
+    unowned var author: Author // unowned 解决循环引用
     
     init(author: Author) {
         self.author = author
@@ -19,7 +19,7 @@ class Video {
 
 class Author {
     var name: String
-    var video: Video?
+    weak var video: Video? // weak 解决循环引用
     
     init(name: String, video: Video? = nil) {
         self.name = name
@@ -35,8 +35,49 @@ var author: Author? = Author(name: "Lebus")
 var video: Video? = Video(author: author!)
 author?.video = video
 
-// 无法释放，最终循环引用
+// 若不添加 weak/unowned 无法释放，最终循环引用
 author = nil
 video = nil
 
 
+// MARK: - delegate 等属性被标记为 weak 的原因，也需放置滥用
+
+class vc: UIViewController, UITableViewDelegate {
+    var tableView: UITableView!
+    override func viewDidLoad() {
+        self.tableView.delegate = self
+    }
+}
+
+
+// MARK: - 捕获列表 CaptureList -- 闭包循环引用的解决方案
+// 1. 普通闭包
+
+class HTMLElement {
+    
+    let name: String
+    let text: String?
+    
+    lazy var asHTML: () -> String = { [unowned self] in
+        if let text = self.text {
+            return "<\(self.name)>\(text)</\(self.name)>"
+        } else {
+            return "<\(self.name) />"
+        }
+    }
+    
+    init(name: String, text: String?) {
+        self.name = name
+        self.text = text
+    }
+    
+    deinit {
+        print("\(name) is being deinitialized")
+    }
+}
+
+var paragraph: HTMLElement? = HTMLElement(name: "p", text: "hello, world")
+print(paragraph!.asHTML())
+paragraph = nil
+
+// 2. 逃逸闭包 -- 见 didFinishPicking
