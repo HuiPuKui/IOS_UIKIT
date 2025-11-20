533db216a64f7b75f52e4198a12fcaad809313fc
huipukui <1137438071@qq.com>
2025-11-21 00:10:11 +0800

    feat: 提交桥接文件

---
+++ 实战-仿小红书/LittlePink/LittlePink/LittlePink-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  LittlePink-Bridging-Header.h
+//  LittlePink
+//
+//  Created by 惠蒲葵 on 2025/11/21.
+//
+
+#ifndef LittlePink_Bridging_Header_h
+#define LittlePink_Bridging_Header_h
+
+#import "MBProgressHUD.h"
+
+#endif /* LittlePink_Bridging_Header_h */
--- 实战-仿小红书/LittlePink/LittlePink/NoteEdit/NoteEditVC.swift
+++ 实战-仿小红书/LittlePink/LittlePink/NoteEdit/NoteEditVC.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import YPImagePicker
+import MBProgressHUD
 
 class NoteEditVC: UIViewController {
 
