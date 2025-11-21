9e7ce18e7ae8328ec12f4f45fa671dc17ab12a8e
huipukui <1137438071@qq.com>
2025-11-22 01:29:20 +0800

    feat: Collection View Cell 的正在拖拽时

--- 实战-仿小红书/LittlePink/LittlePink/NoteEdit/NoteEditVC-DragDrop.swift
+++ 实战-仿小红书/LittlePink/LittlePink/NoteEdit/NoteEditVC-DragDrop.swift
@@ -21,6 +21,17 @@ extension NoteEditVC: UICollectionViewDragDelegate {
 
 extension NoteEditVC: UICollectionViewDropDelegate {
     
+    func collectionView(_ collectionView: UICollectionView, dropSessionDidUpdate session: any UIDropSession, withDestinationIndexPath destinationIndexPath: IndexPath?) -> UICollectionViewDropProposal {
+        // DropProposal -- 放开 cell 的方案
+        // 若需实现 section 间不可拖拽的功能: 可定全局变量 dragingIndexPath，在 itemsForBeginning 中赋值为 indexPath，然后对比 section 是否等于 destinationIndexpath 的 section，若等于则返回 forbidden。
+        
+        if collectionView.hasActiveDrag {
+            return UICollectionViewDropProposal(operation: .move, intent: .insertAtDestinationIndexPath)
+        }
+        
+        return UICollectionViewDropProposal(operation: .forbidden)
+    }
+    
     func collectionView(_ collectionView: UICollectionView, performDropWith coordinator: any UICollectionViewDropCoordinator) {
         
     }
