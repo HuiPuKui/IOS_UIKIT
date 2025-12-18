06ccf6c9a2fcea6985f5d23d555a095352bcbf10
huipukui <1137438071@qq.com>
2025-12-19 00:37:21 +0800

    feat: 一键登录页面UI

--- 实战-仿小红书/LittlePink/LittlePink/Login/LoginVC-LocalLogin.swift
+++ 实战-仿小红书/LittlePink/LittlePink/Login/LoginVC-LocalLogin.swift
@@ -41,13 +41,63 @@ extension LoginVC {
         let config = JVUIConfig()
         config.prefersStatusBarHidden = true
         config.navTransparent = true
-        config.navText = NSAttributedString(string: "")
+        config.navText = NSAttributedString(string: " ")
         
-//        let logoConstraintX = JVLayoutConstraint(attribute: NSLayoutConstraint.Attribute.centerX, relatedBy: NSLayoutConstraint.Relation.equal, to: JVLayoutItem.super, attribute: NSLayoutConstraint.Attribute.centerX, multiplier: 1, constant: 0)
+        let constraintX = JVLayoutConstraint(attribute: NSLayoutConstraint.Attribute.centerX, relatedBy: NSLayoutConstraint.Relation.equal, to: JVLayoutItem.super, attribute: NSLayoutConstraint.Attribute.centerX, multiplier: 1, constant: 0)!
         
-        let logoConstraintY = JVLayoutConstraint(attribute: NSLayoutConstraint.Attribute.top, relatedBy: NSLayoutConstraint.Relation.equal, to: JVLayoutItem.super, attribute: NSLayoutConstraint.Attribute.centerY, multiplier: 1/7, constant: 0)
+        let logoConstraintY = JVLayoutConstraint(attribute: NSLayoutConstraint.Attribute.top, relatedBy: NSLayoutConstraint.Relation.equal, to: JVLayoutItem.super, attribute: NSLayoutConstraint.Attribute.bottom, multiplier: 1/7, constant: 0)!
+        config.logoConstraints = [constraintX, logoConstraintY]
         
-        JVERIFICATIONService.customUI(with: config)
+        let numberConstraintY = JVLayoutConstraint(attribute: NSLayoutConstraint.Attribute.centerY, relatedBy: NSLayoutConstraint.Relation.equal, to: JVLayoutItem.super, attribute: NSLayoutConstraint.Attribute.centerY, multiplier: 1, constant: 35)!
+        config.numberConstraints = [constraintX, numberConstraintY]
+        
+        let sloganConstraintY = JVLayoutConstraint(attribute: NSLayoutConstraint.Attribute.centerY, relatedBy: NSLayoutConstraint.Relation.equal, to: JVLayoutItem.number, attribute: NSLayoutConstraint.Attribute.centerY, multiplier: 1, constant: 35)!
+        config.sloganConstraints = [constraintX, sloganConstraintY]
+        
+        config.logBtnText = "同意协议并一键登录"
+        config.logBtnImgs = [
+            UIImage(named: "localLoginBtn-nor")!,
+            UIImage(named: "localLoginBtn-nor")!,
+            UIImage(named: "localLoginBtn-hig")!
+        ]
+        let logBtnConstraintY = JVLayoutConstraint(attribute: NSLayoutConstraint.Attribute.centerY, relatedBy: NSLayoutConstraint.Relation.equal, to: JVLayoutItem.slogan, attribute: NSLayoutConstraint.Attribute.centerY, multiplier: 1, constant: 50)!
+        config.logBtnConstraints = [constraintX, logBtnConstraintY]
+        
+        config.privacyState = true // 默认勾选
+        config.checkViewHidden = true // 隐藏
+        
+        config.appPrivacyOne = ["用户协议", "https://www.cctalk.com/m/group/86306378"]
+        config.appPrivacyTwo = ["隐私政策", "https://www.cctalk.com/m/group/86308246"]
+        config.privacyComponents = ["登录注册代表你已同意", "以及", "和", " "]
+        config.appPrivacyColor = [UIColor.secondaryLabel, blueColor]
+        config.privacyTextAlignment = .center
+        
+        let privacyConstraintY = JVLayoutConstraint(attribute: NSLayoutConstraint.Attribute.top, relatedBy: NSLayoutConstraint.Relation.equal, to: JVLayoutItem.slogan, attribute: NSLayoutConstraint.Attribute.bottom, multiplier: 1, constant: -70)!
+        let privacyConstraintW = JVLayoutConstraint(attribute: NSLayoutConstraint.Attribute.width, relatedBy: NSLayoutConstraint.Relation.equal, to: JVLayoutItem.none, attribute: NSLayoutConstraint.Attribute.width, multiplier: 1, constant: 260)!
+        config.privacyConstraints = [constraintX, privacyConstraintY, privacyConstraintW]
+        
+        config.agreementNavBackgroundColor = mainColor
+        config.agreementNavReturnImage = UIImage(systemName: "chevron.left")!
+        
+        JVERIFICATIONService.customUI(with: config) { customView in
+            let otherLoginBtn = UIButton()
+            otherLoginBtn.setTitle("其它方式登录", for: .normal)
+            otherLoginBtn.setTitleColor(.secondaryLabel, for: .normal)
+            otherLoginBtn.titleLabel?.font = .systemFont(ofSize: 15)
+            otherLoginBtn.translatesAutoresizingMaskIntoConstraints = false
+            otherLoginBtn.addTarget(self, action: #selector(self.otherLogin), for: .touchUpInside)
+            customView.addSubview(otherLoginBtn)
+            
+            NSLayoutConstraint.activate([
+                otherLoginBtn.centerXAnchor.constraint(equalTo: customView.centerXAnchor),
+                otherLoginBtn.centerYAnchor.constraint(equalTo: customView.centerYAnchor, constant: 170),
+                otherLoginBtn.widthAnchor.constraint(equalToConstant: 279)
+            ])
+        }
+    }
+    
+    @objc private func otherLogin() {
+        
     }
     
     // MARK: - 弹出一键登录授权页 + 用户点击登录后
