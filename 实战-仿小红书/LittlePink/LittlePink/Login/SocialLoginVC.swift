8ae860f67d950b1deec448d2557f8c485fc0d66a
huipukui <1137438071@qq.com>
2025-12-24 00:46:31 +0800

    feat: 添加 Apple 登录按钮

--- 实战-仿小红书/LittlePink/LittlePink/Login/SocialLoginVC.swift
+++ 实战-仿小红书/LittlePink/LittlePink/Login/SocialLoginVC.swift
@@ -12,21 +12,14 @@ class SocialLoginVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func signInWithAlipay(_ sender: Any) {
         self.signInWithAlipay()
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func signInWithApple(_ sender: Any) {
+        
     }
-    */
-
+    
 }
