fee156c0cfa10f7d0743f248010333cdf9fbe8f8
huipukui <1137438071@qq.com>
2026-01-03 11:10:11 +0800

    feat: 暂存笔记详情页

---
+++ 实战-仿小红书/LittlePink/LittlePink/NoteDetail/NoteDetailVC.swift
@@ -0,0 +1,33 @@
+//
+//  NoteDetailVC.swift
+//  LittlePink
+//
+//  Created by 惠蒲葵 on 2026/1/3.
+//
+
+import UIKit
+
+class NoteDetailVC: UIViewController {
+
+    @IBOutlet weak var authorAvatarBtn: UIButton!
+    
+    @IBOutlet weak var authorNickNameBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
