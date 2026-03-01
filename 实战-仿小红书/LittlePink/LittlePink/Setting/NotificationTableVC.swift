10c4c2f315c4cd1733508bb459666dc9bba0709f
huipukui <1137438071@qq.com>
2026-03-01 14:53:10 +0800

    feat: 通知设置UI

---
+++ 实战-仿小红书/LittlePink/LittlePink/Setting/NotificationTableVC.swift
@@ -0,0 +1,18 @@
+//
+//  NotificationTableVC.swift
+//  LittlePink
+//
+//  Created by 惠蒲葵 on 2026/3/1.
+//
+
+import UIKit
+
+class NotificationTableVC: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
